94b1311b32c8b639347aa82d26c85068724242c4
fabsilib <fabio.siliberto@eng.it>
2017-04-21 11:25:51 +0200

    Updating and cleaning gulp task

--- gulpfile.js
+++ gulpfile.js
@@ -13,7 +13,8 @@ var gulp = require('gulp'),
  * Paths
  */
 var paths = {
-    build: "build"
+    build: "build",
+    index: "./"
 };
 
 /*
@@ -23,24 +24,42 @@ var dest = paths.build;
 
 /*
  * This task run in sequence all tasks needed to build files for release.
- * (npm run build)
  */
 gulp.task('build', function (done) {
-    // run sequence tasks
     runSequence(
         'clean',
-        'tsc:index',
-        'tsc:src',
+        'tsc',
         done
     );
 });
 
-gulp.task('clean', function () {
-    del.sync(paths.build);
+gulp.task('clean', function (done) {
+    return runSequence(
+        'clean:index',
+        'clean:build',
+        done
+    )
+});
+
+gulp.task('tsc', function (done) {
+    return runSequence(
+        'tsc:src',
+        'tsc:index',
+        done
+    )
+});
+
+gulp.task('clean:index', function () {
+    del.sync(paths.index + '/index.js');
+    del.sync(paths.index + '/index.d.ts');
+});
+
+gulp.task('clean:build', function () {
+    del.sync(dest);
 });
 
 gulp.task('tsc:index', function () {
-    return gulp.src('index.ts')
+    return gulp.src(paths.index + '/index.ts')
         .pipe(tsIndex({
             module: "commonjs",
             target: "es5",
