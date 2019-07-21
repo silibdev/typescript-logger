13cfe65b2b3638091fd3763e60b2c4ca19d8d486
Fabio Siliberto <fabio.siliberto@gmail.com>
2019-07-21 18:24:05 +0200

    Updated demo structure

---
+++ demo/DemoLogger.js
@@ -0,0 +1,6 @@
+const log = Logger.Log.create('Demo Component');
+log.info('test info');
+log.log('test log');
+log.debug('test debug');
+log.warn('test warning');
+log.error('test error');
--- demo/scripts/DemoLogger.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-require(["scripts/Logger"], function (log_1) {
-    var log = log_1.Log.create('Demo Component');
-    log.info('test info');
-    log.log('test log');
-    log.debug('test debug');
-    log.warn('test warning');
-    log.error('test error');
-});
