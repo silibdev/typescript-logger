341f595379647546513707a61676153c2d8b5def
Fabio Siliberto <fabio.siliberto@gmail.com>
2019-07-31 00:04:45 +0200

    Updated docs comments to Level

--- src/level.ts
+++ src/level.ts
@@ -1,10 +1,25 @@
 /**
- * Supported levels of logging
+ * Supported levels of logging. Each one sets a color as border of the message in the log
  */
 export enum Level {
+    /**
+     * This gives a blue border to the message
+     */
     INFO,
+    /**
+     * This gives a black border to the message
+     */
     LOG,
+    /**
+     * This gives a violet border to the message
+     */
     DEBUG,
+    /**
+     * This gives a orange border to the message
+     */
     WARN,
+    /**
+     * This gives a red border to the message
+     */
     ERROR
 }
