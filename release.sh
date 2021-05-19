883c35aea2b477b95a09fde73d8666c66c7d59f9
silibdev <fabio.siliberto@gmail.com>
2021-05-19 17:27:41 +0200

    Building library and docs for vmajor

--- release.sh
+++ release.sh
@@ -6,8 +6,9 @@ echo "New version:"
 read version
 
 echo "Building library and docs with new version"
-npm run build
+npm run clean
 npm run docs
+npm run build
 
 git add .
 git commit -m "Building library and docs for v${version}"
